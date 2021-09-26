41d41e97f02332f8033682eb1c50fe8122d14bcf
Lee Pham <leepham@utexas.edu>
2021-09-25 23:50:11 -0400

    change variable name

--- convert_24_bit_to_8_bit.py
+++ convert_24_bit_to_8_bit.py
@@ -1,9 +1,9 @@
 from typing import List
 
 
-def convert_24_bit_to_8_bit(L: List[List]) -> List[int]:
+def convert_24_bit_to_8_bit(pixel_list: List[List]) -> List[int]:
     compressed_colors = []
-    for r, g, b in L:
+    for r, g, b in pixel_list:
         compressed_colors.append(((r // 32) << 5) + ((g // 32) << 2) + (b // 64))
 
     return compressed_colors
